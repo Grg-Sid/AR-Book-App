902af0454ba9a4c3fd599a04c98769a1a2c58e59
Grg-Sid <siddhantgurung007@gmail.com>
2023-06-01 15:11:53 +0530

    Add the a basic UI for AR Scenes and Made the 0.1 build

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWZTaVhuYi8vLy8vQUFBQm1kQk9jTFB0SWsyQmtmRkhjb05WTE9RRVoyQmlKT3loT25SSkFpZ0tuTDVTdkx4SmZJTWFQK0VHL05zVDhOL1ZpSDF1a3AxckY2b0pBNWlEQUJVNVhzMmE4cXlFaHp5aEZtRURFcHBUemhMOTU2MEtnSGJZZmRIMlNNWnhkUUsxWVBRMjk0OW0wMWJJdmF3L1c5SUZFckFTczJ3RHlXY2RmZ0c3RWRXd2pqSmU1RWpNaU52Mk93QjFxMnpFVDd4Wm0yaURObVBRbGhkUFFuTGZhbUhQRFJVTlg5N0FKMTl0SUo3dElCck5ucTVLWmFwWTlxUGxsVVQ3QjArd2pkaWNsS2tjUGRyZWQvaTZlWlRFS2JZbEwzNDY4clFIV0JxU3lCQTdHSm5sQTBzK1VmdnhsQkI3dVhxU1RtNm5MN3V4VGNsMG1KbDRkUEdpdEplbWI3dVA1VWhVWmFoc2JVUnJib2pwTlAvRjRGNnk=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
