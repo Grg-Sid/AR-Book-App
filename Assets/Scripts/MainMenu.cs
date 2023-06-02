95585cdb9e5a03e9108bb4ba481741cccff10eaa
Grg-Sid <siddhantgurung007@gmail.com>
2023-06-02 21:59:09 +0530

    Add Backbutton functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void SceneLoader(string sceneName)
+    int sceneIndex; 
+    int currentIndex;
+
+    private void Start()
     {
-        SceneManager.LoadScene(sceneName);
-        Debug.Log("Scene changed");
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        currentIndex = sceneIndex;
+        //Debug.Log("Working");
     }
 
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Esc was pressed");
+            if (currentIndex == 0)
+            {
+                Application.Quit();
+                Debug.Log("App has Quit");
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneIndex - 1);
+                currentIndex = sceneIndex - 1;
+                Debug.Log("Scene Changed");
+            }
+        }
+        if (!Input.GetKeyDown(KeyCode.Escape))
+        {
+            return;
+        }
+    }
+
+    public void SceneLoaderNext()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+        currentIndex = sceneIndex + 1;
+        Debug.Log(currentIndex);
+    }
+    public void SceneLoaderPrevious()
+    {
+        SceneManager.LoadScene(sceneIndex - 1);
+        currentIndex = sceneIndex - 1;
+        Debug.Log(currentIndex);
+    }
     public void QuitApp()
     {
         Application.Quit();
